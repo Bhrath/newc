2fde875f25fe8d4d0d67910d8f63a2351d6df267
Cloud User <ec2-user@ip-172-31-17-98.us-east-2.compute.internal>
2019-04-10 10:43:36 +0000

    modifying fact.c file

--- factorial.c
+++ factorial.c
@@ -22,3 +22,45 @@ int main()
 
     return 0;
 }
+
+
+
+http://repos.del.extreme-ix.org/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.nbrc.ac.in/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos.mirror.snu.edu.in/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos.mirrors.estointernet.in/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://ftp.iitm.ac.in/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+
+Other mirrors further away:
+
+http://centos.myfahim.com/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.dhakacom.com/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.xeonbd.com/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.ehost.vn/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos-hcm.viettelidc.com.vn/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos-hn.viettelidc.com.vn/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.horizon.vn/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirrors.viethosting.com/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirrors.123host.vn/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos.nethub.com.hk/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos.01link.hk/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://ftp.cuhk.edu.hk/pub/Linux/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror-hk.koddos.net/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.sunnyvision.com/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.xtom.com.hk/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://repo.virtualhosting.hk/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.vpshosting.com.hk/pub/linux/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos.communilink.net/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos.uhost.hk/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://centos.usonyx.net/main/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.vastspace.net/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.nus.edu.sg/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.vodien.com/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.newmediaexpress.com/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
+http://mirror.0x.sg/centos/7.6.1810/isos/x86_64/CentOS-7-x86_64-Minimal-1810.iso
++ others, see the full list of mirrors: https://www.centos.org/download/mirrors/
+
+Mirrors verified Wed Apr 10 05:03:09 2019 UTC
+
+You can also download the ISO images using bittorrent, a peer-to-peer file sharing protocol. The .torrent files can be found from CentOS mirrors. Various bittorrent clients are available, including (in no particular order of preference): utorrent, vuze (Azureus), BitTorrent, Deluge, ctorrent, ktorrent, rtorrent and transmission. Packaged copies of various torrent clients for CentOS can be found in the repositories listed in the following wiki article: https://wiki.centos.org/AdditionalResources/Repositories 
+ 
